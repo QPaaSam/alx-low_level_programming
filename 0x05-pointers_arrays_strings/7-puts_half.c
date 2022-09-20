037a5442d24c794b1a4d4b52be778d31ec14a72f
QPaaSam <qaugustine08@gmail.com>
2022-09-19 18:06:34 -0700

    7-puts hals

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,14 +6,24 @@
  */
 void puts_half(char *str)
 {
-int len = 0, i, n;
-while (str[len] != '\0')
+int len, n, i;
+len = 0;
+while (str[len] != '\0'	
+{
 len++;
+}
 if (len % 2 == 0)
-n = len / 2;
-else
-n = (len 0 + 1) / 2;
-for (i = n; i < len; i++)
+{
+for (i = len / 2; str[i] != '\0'; i++)
+{
 _putchar(str[i]);
+}
+} else if (len % 2)
+{
+for (n = (len - 1) / 2; n < len - 1; n++)
+{
+_putchar(str[n + 1]);
+}
+}
 _putchar('\n');
 }
